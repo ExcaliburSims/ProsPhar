5f6c71ba4db7e899346505ad8a86ef7db82818b8
Siméon zilu <50544380+ExcaliburSims@users.noreply.github.com>
2023-05-09 10:47:39 +0100

    feat: add login frame

--- src/prosphar/Prosphar.java
+++ src/prosphar/Prosphar.java
@@ -4,6 +4,8 @@
  */
 package prosphar;
 
+import prosphar.gui.gui_form.Login;
+
 /**
  *
  * @author user
@@ -15,6 +17,8 @@ public class Prosphar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login=new Login();
+        login.setVisible(true);
     }
     
 }
